99a9980613f8a8778691efb736e584fde99a7e10
qmt8843 <qmt8843@rit.edu>
2024-11-01 02:42:17 -0400

    fixed more formatting

--- setup-fileshare.sh
+++ setup-fileshare.sh
@@ -5,7 +5,7 @@ addr=`/sbin/ifconfig eth0 | grep "inet " | cut -d: -f2 | awk '{print $2}'`
 echo "\$urlGet = \"$addr\"" | cat - setup-powerview.ps1 > tmp.txt && mv tmp.txt setup-powerview.ps1
 
 git clone https://github.com/samratashok/ADModule
-echo "\$urlGet = $addr" | cat - setup-admodule.ps1 > tmp.txt && mv tmp.txt setup-admodule.ps1
-echo '$destGet = \"C:\\Users\\robb.stark\\test\"' | cat - setup-admodule.ps1 > tmp.txt && mv tmp.txt setup-admodule.ps1
+echo "\$urlGet = \"$addr\"" | cat - setup-admodule.ps1 > tmp.txt && mv tmp.txt setup-admodule.ps1
+echo '$destGet = "C:\\Users\\robb.stark\\test"' | cat - setup-admodule.ps1 > tmp.txt && mv tmp.txt setup-admodule.ps1
 
 python3 -m http.server 8080
