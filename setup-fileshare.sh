9f297ba6aa54c7b9a3887a4b6137bdb1b537f3f8
qmt8843 <qmt8843@rit.edu>
2024-11-01 02:27:36 -0400

    testing addr 10

--- setup-fileshare.sh
+++ setup-fileshare.sh
@@ -6,6 +6,6 @@ echo "\$urlGet = $addr" | cat - setup-powerview.ps1 > tmp.txt && mv tmp.txt setu
 
 git clone https://github.com/samratashok/ADModule
 echo "\$urlGet = $addr" | cat - setup-admodule.ps1 > tmp.txt && mv tmp.txt setup-admodule.ps1
-echo "\$urlGet = C:\Users\robb.stark\test" #| cat - setup-admodule.ps1 > tmp.txt && mv tmp.txt setup-admodule.ps1
+echo '$destGet = C:\\Users\\robb.stark\\test' cat - setup-admodule.ps1 > tmp.txt && mv tmp.txt setup-admodule.ps1
 
 python3 -m http.server 8080
