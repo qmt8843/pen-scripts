f7834c14a677828d44d0c193aca87682db58a1ba
qmt8843 <qmt8843@rit.edu>
2024-11-01 02:15:35 -0400

    testing addr 4

--- setup-fileshare.sh
+++ setup-fileshare.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 git clone https://github.com/PowerShellMafia/PowerSploit.git
-addr=`/sbin/iconfig eth0 | grep "inet " | cut -d: -f2 | awk '{print $2}'`
+addr=`/sbin/ifconfig eth0 | grep "inet " | cut -d: -f2 | awk '{print $2}'`
 echo $addr
 
 git clone https://github.com/samratashok/ADModule
